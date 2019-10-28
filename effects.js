dfeee719c9499d4667600792229126bffe06685d
Jamie Reardon <decor1878@gmail.com>
2019-10-28 12:00:00 +0000

    Add arrow functions, animate scoreboard on reset

--- effects.js
+++ effects.js
@@ -5,7 +5,7 @@ const winScreen = () => {
   $('.btn__reset').hide();
   // Fade and slide out the scoreboard
   $('#scoreboard').fadeOut(700);
-  scoreboard.style = "transition: transform .5s cubic-bezier(0.950, 0.050, 0.795, 0.035); transform: translateY(100%);";
+  scoreboard.style = 'transition: transform .5s cubic-bezier(.95,.05,.795,.035); transform: translateY(100%);';
   // Fade out the letter ul element
   $('#phrase ul').delay(1000).fadeOut();
   // Fade In win screen
@@ -13,21 +13,27 @@ const winScreen = () => {
   // Update text content
   $('.title').text('You Guessed It!');
   // Add a h3 element to the page
-  let h3 = document.createElement('h3');
+  const h3 = document.createElement('h3');
   h3.innerText = ('Play Again?');
   screen.appendChild(h3);
   // Show the start button with updated text
   $('.btn__reset').show().text('Reset Game');
   // Add an event listener to the button to reset the game
-  $('.btn__reset').click( function() {
+  $('.btn__reset').click( () => {
+    // Reset the game
     resetPhrase();
     resetButtons();
     resetTries();
     getRandomPhraseAsArray(phrases);
     addPhraseToDisplay(randomPhrase);
+    // Hide the phrase div and fade in with a delay
     $('#phrase').hide().delay(600).fadeIn(1500, 'linear');
+    // Show the phrase
     $('#phrase ul').show();
-    scoreboard.style = "transform: translateX(0); opacity: 1; display: block;";
+    // Fade in the scoreboard
+    $('#scoreboard').fadeIn(700);
+    // Reset the style of the scoreboard
+    scoreboard.style = 'transition: transform .5s cubic-bezier(.24,.34,.9,.8); transform: translateX(0); opacity: 1; display: block;';
   });
 }
 
@@ -38,7 +44,7 @@ const loseScreen = () => {
   $('.btn__reset').hide();
   // Fade and slide out the scoreboard
   $('#scoreboard').fadeOut(700);
-  scoreboard.style = "transition: transform .5s cubic-bezier(0.950, 0.050, 0.795, 0.035); transform: translateY(100%);";
+  scoreboard.style = 'transition: transform .5s cubic-bezier(.95,.05,.795,.035); transform: translateY(100%);';
   // Fade out the letter ul element
   $('#phrase ul').delay(1000).fadeOut();
   // Fade in lose screen
@@ -46,25 +52,32 @@ const loseScreen = () => {
   // Update text content
   $('.title').text('Boo! Go Watch More Movies!');
   // Add a h3 element to the page
-  let h3 = document.createElement('h3');
+  const h3 = document.createElement('h3');
   h3.innerText = ('Dare To Give It Another Go?');
   screen.appendChild(h3);
   // Show the start button with updated text
   $('.btn__reset').show().text('Try Again');
   // Add an event listener to the button to reset the game
-  $('.btn__reset').click( function() {
+  $('.btn__reset').click( () => {
+    // Reset the game
     resetPhrase();
     resetButtons();
     resetTries();
     getRandomPhraseAsArray(phrases);
     addPhraseToDisplay(randomPhrase);
+    // Hide the phrase div and fade in with a delay
     $('#phrase').hide().delay(600).fadeIn(1500, 'linear');
+    // Show the phrase
     $('#phrase ul').show();
-    scoreboard.style = "transform: translateX(0); opacity: 1; display: block;";
+    // Fade in the scoreboard
+    $("#scoreboard").fadeIn(700);
+    // Reset the style of the scoreboard
+    scoreboard.style = 'transition: transform .5s cubic-bezier(.24,.34,.9,.8); transform: translateX(0); opacity: 1; display: block;';
   });
 }
 
-$('.btn__reset').click(function() {
+// Add an event listener on the start button for a click event
+$('.btn__reset').click( () => {
   // Hide the Start screen overlay
   $('#overlay').fadeOut(600, 'linear');
   // Hide the phrase and fade it in
